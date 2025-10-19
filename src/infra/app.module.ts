7508f2f1549e97e69dc9a655c4df7c0c662e6cc1
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-10-19 08:49:23 -0300

    feat: :sparkles: add http module

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,6 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from './http/http.module';
+import { EnvModule } from './env/env.module';
+import { envSchema } from './env/env';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    HttpModule,
+    EnvModule,
+  ],
 })
 export class AppModule {}
